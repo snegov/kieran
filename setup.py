6dc2b85236dc628fe1f62df1b67a12f9520cabc2
Maks Snegov <snegov@spqr.link>
2020-09-11 00:09:49 +0300

    Use find_namespace_packages in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name="spqr.kieran",
@@ -6,6 +6,6 @@ setup(
     author="Maks Snegov",
     author_email="snegov@spqr.link",
     description="Helpers for SPQR projects",
-    packages=find_packages(),
+    packages=find_namespace_packages(include=['spqr.*']),
     python_requires=">=3.8",
 )
